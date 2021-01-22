487d02f2577e4d9643aebe101eb63c1cd28be7d0
JongwonAhn <azsx1001202@naver.com>
2021-01-22 22:03:41 +0900

    .

---
+++ app/src/main/java/myselfStudy/study01.java
@@ -0,0 +1,38 @@
+package myselfStudy;
+
+import java.util.Scanner;
+
+public class study01 {
+  public static void main(String[] args) {
+    Scanner keyScan = new Scanner(System.in);
+
+    int i;
+    int j;
+    int k = -1;
+
+    int[] array = new int[3];
+    while(true) {
+      for ( i = 0; i < array.length; i++) {
+
+        array[i] = keyScan.nextInt();
+
+
+        array[i-1 ] = array[i];
+
+
+
+        for (j=0; j < array.length; j++)
+
+
+          System.out.print(array[j]);
+        System.out.println("");
+
+
+      }
+
+
+    }
+
+  }
+
+}
---
+++ app/src/main/java/myselfStudy/study02.java
@@ -0,0 +1,50 @@
+package myselfStudy;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class study02 {
+  static Scanner keyScan = new Scanner(System.in);
+  public static class Member{
+
+    int a;
+    String b;
+    Date c;
+
+    public String info() {
+      this.a=40;
+      this.b="김";
+      this.c= new java.sql.Date(System.currentTimeMillis());
+
+      return a+b+c;
+    }
+
+    public Member add(int a,String b) {
+      return this;
+
+    }
+  }
+
+  public static void main(String[] args) {
+
+    int LENGTH = 100;
+
+    Member[] Members = new Member[LENGTH];
+
+    Members[0]=new Member();
+
+    Member member = new Member();
+
+    Members[0]=member;
+
+    member.a = 20;
+    member.b = "ga";
+    member.c = new java.sql.Date(System.currentTimeMillis());
+
+
+    System.out.println( member.info());
+
+  }
+}
+
+
