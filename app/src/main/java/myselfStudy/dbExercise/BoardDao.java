80c9ecb06a4cf3f3db91ff7b03d2f324d0419926
JongwonAhn <azsx1001202@naver.com>
2021-03-25 20:35:20 +0900

    .

---
+++ app/src/main/java/myselfStudy/dbExercise/BoardDao.java
@@ -0,0 +1,12 @@
+package myselfStudy.dbExercise;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BoardDao {
+  public int delete(int no) throws Exception {
+    try (Connection con = DriverManager.getConnection(
+        ));
+  }
+
+}
